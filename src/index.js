477aa6fe8b8bc9478ddf84971096a7edf7801003
miShelbyT <mi.shelbyrose@gmail.com>
2020-12-02 11:43:14 -0500

    did some additional rendering, working on event listener for pop up

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const petContainer = document.querySelector('#pet-collection')
+// const petButton = document.querySelector(".card")
 
 function fetchPets() { 
 fetch("http://localhost:3000/api/v1/pets")
@@ -10,18 +11,30 @@ fetch("http://localhost:3000/api/v1/pets")
 })
 }
 
+// EVENT HANDLERS
+const petPopUp = (e) => {
+  console.log(e.target)
+}
+
+
 
 const renderPets = (pet) => {
     const petUl = document.createElement('ul')
     const petImg = document.createElement('img')
+    const petName = document.createElement('h2')
+    const petType = document.createElement('h4')
 
     petUl.className = "card"
+    petName.textContent = pet.name
+    petType.textContent = `Breed: ${pet.breed}`
     
     petImg.src = pet.img 
     petImg.alt = pet.name
     petImg.className = "pet-avatar"
 
-    petUl.append(petImg)
+    petUl.addEventListener("click", (e) => petPopUp(e))
+
+    petUl.append(petImg, petName, petType)
     petContainer.append(petUl)
 
 }
