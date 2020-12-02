69e3be7d9adda5de0f9368367a5e5d611ef911c3
miShelbyT <mi.shelbyrose@gmail.com>
2020-12-02 17:35:24 -0500

    started playdate functionality

--- src/index.js
+++ src/index.js
@@ -1,92 +1,145 @@
 //Original variables
-const petContainer = document.querySelector("#pet-collection");
-const uRL = "http://localhost:3000/api/v1/pets";
+const petContainer = document.querySelector("#pet-collection")
+const showPet = document.querySelector("#show-pet")
 
+const uRL = "http://localhost:3000/api/v1/pets"
+
+// INITIAL RENDER
 function fetchPets() {
   fetch(uRL)
     .then((resp) => resp.json())
     .then((petArray) => {
       petArray.forEach((pet) => {
-        renderPets(pet);
-      });
-    });
+        renderPets(pet)
+      })
+    })
 }
 
 // EVENT HANDLERS
+// fetch sends details of petObj so we can render to top div
 const petDetails = (e) => {
   if (e.target.matches(".deets")) {
-    const id = e.target.dataset.id;
-    console.log(id);
+    const id = e.target.dataset.id
+    // console.log(id);
     fetch(`${uRL}/${id}`)
       .then((resp) => resp.json())
       .then((petObj) => {
-        renderPet(petObj);
-        console.log(petObj);
+        renderPet(petObj)
+        // console.log(petObj)
       });
   }
 };
 
+// renderPet renders inside top div to make edit/PATCH, possibly delete
 const renderPet = (petObj) => {
-  showPet = document.querySelector("#show-pet");
+  const showPet = document.querySelector("#show-pet")
+  const petName = document.createElement("h2")
+  const petBreed = document.createElement("h3")
+  const petAge = document.createElement("h3")
+  const showPetText = document.createElement("p")
+  const petImg = document.createElement("img")
+  const petPersonality = document.createElement("h4")
+  // variable created so we can append playdates
+  const allDates = document.createElement("ul")
+  const makePlaydateBtn = document.createElement("button")
+
   showPet.innerHTML = ""
+  petName.textContent = `Name: ${petObj.name}`
+  petBreed.textContent = `Breed: ${petObj.breed}`
+  petImg.src = petObj.img
+  petPersonality.textContent = `Personality: ${petObj.temper}`
+  allDates.textContent = "Playdates"
+  makePlaydateBtn.textContent = "Wanna Play?"
+  makePlaydateBtn.dataset.id = petObj.id
 
-  showPetText = document.createElement("p")
 
-  const petName = document.createElement("h2");
-  petName.textContent = petObj.name;
-
-  const petBreed = document.createElement("h3");
-  petBreed.textContent = petObj.breed;
-
-  const petAge = document.createElement("h3");
   if (petObj.age > 1) {
-    petAge.textContent = `${petObj.age} years`;
+    petAge.textContent = `Age: ${petObj.age} years old`
   } else {
-    petAge.textContent = `${petObj.age} year`;
+    petAge.textContent = `Age; ${petObj.age} year old`
   }
 
-  
 
-  const petImg = document.createElement("img")
-  petImg.src = petObj.img
+  // iterates thru to render playdate info
+  function playDates(petObj) {
+    petObj.playdates.forEach(playdate => {
+      const date = document.createElement("li")
+      // if (playdate !== [{}]) {
 
-  const petPersonality = document.createElement("h4");
-  petPersonality.textContent = petObj.temper;
+      date.textContent = `Date: ${playdate.date}, Location: ${playdate.location}`
+      allDates.append(date)
+      // }
+      // else {
+      //   date.textContent = "I don't have any playdates scheduled."
+      //   allDates.append(date)
+      // }
+      // console.log(playdate)
 
-  showPetText.append( petName, petBreed, petAge, petPersonality);
-  showPet.append(petImg, showPetText)
+    })
+  }
+
+  playDates(petObj)
+
+  showPetText.append(petName, petBreed, petAge, petPersonality)
+  showPet.append(petImg, showPetText, allDates, makePlaydateBtn)
 };
 
 // EVENT LISTENERS
+petContainer.addEventListener("click", petDetails)
+// update button
+showPet.addEventListener("click", (event) => {
+  if (event.target.matches("button")) {
+    const id = event.target.dataset.id
+    console.log(id)
 
-petContainer.addEventListener("click", petDetails);
+    const newPlaydate = {
+      petId: id,
 
+
+    }
+
+    fetch('http://localhost:3000/api/v1/playdates', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify(newPlaydate),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+      })
+
+  }
+})
+
+
+// render all pets to page
 const renderPets = (pet) => {
-  const petDiv = document.createElement("div");
-  const petImg = document.createElement("img");
-  const petName = document.createElement("h2");
-  const petType = document.createElement("h4");
-  const petButton = document.createElement("button");
+  const petDiv = document.createElement("div")
+  const petImg = document.createElement("img")
+  const petName = document.createElement("h2")
+  const petType = document.createElement("h4")
+  const petButton = document.createElement("button")
 
-  petDiv.className = "card";
+  petDiv.className = "card"
 
-  petName.textContent = pet.name;
-  petType.textContent = `Breed: ${pet.breed}`;
+  petName.textContent = pet.name
+  petType.textContent = `Breed: ${pet.breed}`
 
-  petButton.classList.add("deets");
+  petButton.classList.add("deets")
 
-  petButton.textContent = "Pet Deets";
-  petButton.dataset.id = pet.id;
+  petButton.textContent = "Pet Deets"
+  petButton.dataset.id = pet.id
 
-  petImg.src = pet.img;
-  petImg.alt = pet.name;
-  petImg.className = "pet-avatar";
+  petImg.src = pet.img
+  petImg.alt = pet.name
+  petImg.className = "pet-avatar"
 
-  // petUl.addEventListener("click", (e) => petPopUp(e.target["data-id"]))
+  petDiv.append(petImg, petName, petButton, petType)
 
-  petDiv.append(petImg, petName, petButton, petType);
+  petContainer.append(petDiv)
+}
 
-  petContainer.append(petDiv);
-};
-
-fetchPets();
+fetchPets()
